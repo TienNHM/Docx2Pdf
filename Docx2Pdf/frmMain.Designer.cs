2263ce213778a9b07a0cbfdf2cc6e9ba383a3d7b
Nguyễn Huỳnh Minh Tiến <33385777+Titytus@users.noreply.github.com>
2020-01-09 12:13:26 +0700

    add

---
+++ Docx2Pdf/frmMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Docx2Pdf
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btStart = new System.Windows.Forms.Button();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btEnd = new System.Windows.Forms.Button();
+            this.btSelect = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btStart.Location = new System.Drawing.Point(119, 46);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(68, 26);
+            this.btStart.TabIndex = 0;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font(".VnRevueH", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.lbTitle.ForeColor = System.Drawing.Color.Maroon;
+            this.lbTitle.Location = new System.Drawing.Point(62, 11);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(166, 31);
+            this.lbTitle.TabIndex = 1;
+            this.lbTitle.Text = "DOCX TO PDF";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 78);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(261, 21);
+            this.progressBar.TabIndex = 2;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lbStatus.Location = new System.Drawing.Point(245, 102);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(24, 13);
+            this.lbStatus.TabIndex = 3;
+            this.lbStatus.Text = "0 %";
+            this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btEnd
+            // 
+            this.btEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btEnd.Location = new System.Drawing.Point(193, 46);
+            this.btEnd.Name = "btEnd";
+            this.btEnd.Size = new System.Drawing.Size(81, 26);
+            this.btEnd.TabIndex = 4;
+            this.btEnd.Text = "End task";
+            this.toolTip.SetToolTip(this.btEnd, "Press End task to kill this task. Please wait app for respondding!");
+            this.btEnd.UseVisualStyleBackColor = true;
+            this.btEnd.Click += new System.EventHandler(this.btEnd_Click);
+            // 
+            // btSelect
+            // 
+            this.btSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btSelect.Location = new System.Drawing.Point(12, 46);
+            this.btSelect.Name = "btSelect";
+            this.btSelect.Size = new System.Drawing.Size(101, 26);
+            this.btSelect.TabIndex = 5;
+            this.btSelect.Text = "Select files";
+            this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 121);
+            this.Controls.Add(this.btSelect);
+            this.Controls.Add(this.btEnd);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.btStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Docx2Pdf";
+            this.toolTip.SetToolTip(this, "Select files >> Start");
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btEnd;
+        private System.Windows.Forms.Button btSelect;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ Docx2Pdf/frmMain.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace Docx2Pdf
+{
+    public partial class frmMain : Form
+    {
+        public Document Document { get; set; }
+        private OpenFileDialog files;
+        public frmMain()
+        {
+            InitializeComponent();
+            btStart.Enabled = false;
+            btEnd.Enabled = false;
+        }
+
+        private void btSelect_Click(object sender, EventArgs e)
+        {
+            progressBar.Value = 0;
+            lbStatus.Text = "0 %";
+            files = new OpenFileDialog()
+            {
+                Multiselect = true
+            };
+            if (files.ShowDialog() == DialogResult.OK)
+                btStart.Enabled = true;
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            btStart.Enabled = false;
+            btEnd.Enabled = true;
+            DialogResult start = MessageBox.Show("Start this process?", "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (start == DialogResult.Yes)
+                backgroundWorker.RunWorkerAsync();
+        }
+        private void btEnd_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you want to end this task?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialog == DialogResult.Yes)
+            {
+                lbStatus.Text = "Please wait for killing this task...";
+                backgroundWorker.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            app.ShowWindowsInTaskbar = false;
+            string[] listFiles = files.FileNames;
+            int i = 0;
+            foreach (var path in listFiles)
+            {
+                backgroundWorker.ReportProgress((++i * 100) / listFiles.Length);
+                Document = app.Documents.Open(FileName: @path);
+                string name = path.Substring(0, path.LastIndexOf('.'));
+                Document.ExportAsFixedFormat(@name + ".pdf", WdExportFormat.wdExportFormatPDF);
+                app.Documents.Close();
+                if (backgroundWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            app.Quit();
+            backgroundWorker.ReportProgress(100);
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lbStatus.Text = progressBar.Value + " %";
+        }
+        
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btEnd.Enabled = false;
+            if (e.Cancelled)
+            {
+                lbStatus.Text = progressBar.Value + " %";
+                MessageBox.Show("Task cancelled!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (e.Error != null)
+                MessageBox.Show("Error(s) occurred!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                MessageBox.Show("Done!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                progressBar.Value = 0;
+                lbStatus.Text = "0 %";
+            }
+        }
+    }
+}
